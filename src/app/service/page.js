073e6bb5ade383d36ae736c863d19f8689b0d64c
PoohInwZa001 <mariomaolereiei@gmail.com>
2026-06-19 15:47:26 +0700

    version0.1

---
+++ src/app/service/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Service() {
+return (
+    <div>
+    <center>  
+      <Link href="/Home"> Home </Link> | <Link href="/about"> About </Link> | <Link href="/service"> Service </Link> | <Link href="/contact"> Contact </Link>  </center>
+   <div><center> Service page </center> </div>
+   </div>
+ );
+}
